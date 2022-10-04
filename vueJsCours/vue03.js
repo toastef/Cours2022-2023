e4ceb97d3c4c51e2d1d5f4044eaf9ff997e7e50d
toastef <94532614+toastef@users.noreply.github.com>
2022-10-04 09:26:00 +0200

    upd

--- vueJsCours/vue03.js
+++ vueJsCours/vue03.js
@@ -9,13 +9,13 @@ const app = Vue.createApp ({
     },
     methods: {
         ajouterLoisir: function (){
-            if(this.nouveauLoisir == "") return; // si la case est vide on ne peut pas ajouter
+            if(this.nouveauLoisir === "") return; // si la case est vide on ne peut pas ajouter
             if(this.loisirs.findIndex(x => x === this.nouveauLoisir) >=0 ) return; // si il existe déja il ne l'ajoute pas
             this.loisirs.push(this.nouveauLoisir); // ajout du loisir au tableau
             this.nouveauLoisir =""; // remise a blanc de la case newloisir afin de pas faire d'ajout multiple
         }
     }
-});
+});dit
 app.mount('#app'); // liaison entre le app du doc html et le app du vueJs
 
 
